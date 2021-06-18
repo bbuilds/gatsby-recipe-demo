8513a8b92ab2f44bc31bc98aaa7e1bbf4adb8a9b
Branden Youngs <hello@brandenbuilds.com>
2021-06-18 16:21:51 -0400

    created all pages

---
+++ src/pages/tags.js
@@ -0,0 +1,11 @@
+import React from "react"
+import Layout from "../components/Layout"
+const tags = () => {
+  return (
+    <Layout>
+      <h1>My Tages Page</h1>
+    </Layout>
+  )
+}
+
+export default tags
