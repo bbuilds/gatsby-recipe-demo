ba8397bd1d267c2ea0cde7100f5c3aab94779826
Branden Youngs <hello@brandenbuilds.com>
2021-06-20 12:47:25 -0400

    set up contenful

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,12 +4,49 @@
  * See: https://www.gatsbyjs.com/docs/gatsby-config/
  */
 
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
 module.exports = {
   /* Your site config here */
+  siteMetadata: {
+    title: "Simply Da Bomb Recipes",
+    description: "The best recipes out there",
+    author: "@byoungz",
+    person: { name: "branden", age: 32 },
+    simpleData: ["item 1", "item 2"],
+    complexData: [
+      { name: "john", age: 32 },
+      { name: "susan", age: 21 },
+    ],
+  },
   plugins: [
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/assets/images`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `styles`,
+        path: `${__dirname}/src/assets/css`,
+      },
+    },
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: `spqb3wh3z3b8`,
+        // Learn about environment variables: https://gatsby.dev/env-vars
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
+    },
   ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,28 @@
 import React from "react"
 import Layout from "../components/Layout"
+import { StaticImage } from "gatsby-plugin-image"
+
 export default function Home() {
   return (
     <div>
       <Layout>
-        <h1>Hello world!</h1>
+        <main className="page">
+          <header className="hero">
+            <StaticImage
+              src="../assets/images/main.jpeg"
+              alt="eggs"
+              className="hero-img"
+              placeholder="tracedSVG"
+              layout="fullWidth"
+            />
+            <div className="hero-container">
+              <div className="hero-text">
+                <h1>Simple Recipes</h1>
+                <h4>no fucking fluff</h4>
+              </div>
+            </div>
+          </header>
+        </main>
       </Layout>
     </div>
   )
